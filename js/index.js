971a810ae30d8d5c0925de0c157fdeb0d6033e0f
Shantel Brown <Shantel.Brown2021@gmail.com>
2020-04-06 19:43:41 -0700

    All content on web page

--- js/index.js
+++ js/index.js
@@ -52,6 +52,44 @@ snippet.setAttribute('src', siteContent["cta"]["img-src"]);
 let middle = document.getElementById("middle-img");
 middle.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
  
-let heading = document.querySelector("h1");
-// heading.textContent = siteContent.h1;
-console.log(heading)
+let heading = document.querySelector(".cta h1");
+heading.textContent = siteContent["cta"].h1;
+
+
+let btn = document.querySelector(".cta button");
+btn.textContent = siteContent.cta.button;
+
+let title = document.querySelectorAll(".main-content h4");
+title[0].textContent = siteContent["main-content"]["features-h4"]
+// title[0].textContent = 2+2;
+
+title[1].textContent = siteContent["main-content"]["about-h4"]
+
+title[2].textContent = siteContent["main-content"]["services-h4"]
+
+title[3].textContent = siteContent["main-content"]["product-h4"]
+
+title[4].textContent = siteContent["main-content"]["vision-h4"]
+
+let paragraph = document.querySelectorAll(".main-content p");
+paragraph[0].textContent = siteContent["main-content"]["features-content"]
+
+paragraph[1].textContent = siteContent["main-content"]["about-content"]
+
+paragraph[2].textContent = siteContent["main-content"]["services-content"]
+
+paragraph[3].textContent = siteContent["main-content"]["product-content"]
+
+paragraph[4].textContent = siteContent["main-content"]["vision-content"]
+
+document.querySelector(".contact h4").textContent = siteContent.contact["contact-h4"]
+// contact.textContent = siteContent.contact["contact-h4"]
+
+document.querySelectorAll(".contact p")[0].textContent = siteContent.contact.address
+
+document.querySelectorAll(".contact p")[1].textContent = siteContent.contact.phone
+
+document.querySelectorAll(".contact p")[2].textContent = siteContent.contact.email
+
+document.querySelector("footer p").textContent = siteContent.footer.copyright
+console.log(siteContent)
